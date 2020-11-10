79c9cd74bdccef838c79259f3d7697bf648682aa
Vladislav <vkoval2002@gmail.com>
2020-11-10 14:24:49 +0300

    Added PlacementManager that rotates edges

---
+++ src/main/java/com/dawist_o/graphview/placementutilities/PlacementManager.java
@@ -0,0 +1,31 @@
+package com.dawist_o.graphview.placementutilities;
+
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.value.ObservableDoubleValue;
+import javafx.geometry.Point2D;
+
+import static javafx.beans.binding.Bindings.createDoubleBinding;
+
+public class PlacementManager {
+    public static Point2D rotate(Point2D point, Point2D center, double degree) {
+        double angle = Math.toRadians(degree);
+
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+
+        Point2D result = point.subtract(center);
+
+        Point2D rotated = new Point2D(result.getX() * cos - result.getY() * sin,
+                result.getX() * sin + result.getY() * cos);
+
+        return rotated .add(center);
+    }
+
+    public static DoubleBinding atan2(final ObservableDoubleValue y, final ObservableDoubleValue x) {
+        return createDoubleBinding(() -> Math.atan2(y.get(), x.get()), y, x);
+    }
+
+    public static DoubleBinding toDegrees(final ObservableDoubleValue angrad) {
+        return createDoubleBinding(() -> Math.toDegrees(angrad.get()), angrad);
+    }
+}
