0fe7eddf217ea401b4cff751f4054b11bea6c2ec
Vladislav <vkoval2002@gmail.com>
2020-11-19 19:45:06 +0300

    Generics added

--- src/main/java/com/dawist_o/graphview/placementstrategies/PlacementStrategy.java
+++ src/main/java/com/dawist_o/graphview/placementstrategies/PlacementStrategy.java
@@ -6,7 +6,7 @@ import com.dawist_o.model.Graph;
 
 import java.util.Collection;
 
-public interface PlacementStrategy<V> {
+public interface PlacementStrategy<V,E> {
 
-    void place(double width, double height, Graph theDGraph, Collection<VertexNode<V>> vertices);
+    void place(double width, double height, Graph<V,E> theDGraph, Collection<VertexNode<V>> vertices);
 }
