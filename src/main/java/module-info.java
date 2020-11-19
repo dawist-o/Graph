33f0ae4f459b88b64c6270481bf9346cc34396c8
Vladislav <vkoval2002@gmail.com>
2020-11-19 19:47:07 +0300

    New exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,15 @@ module Graph {
     exports com.dawist_o.controllers;
     opens com.dawist_o.controllers;
 
+    exports com.dawist_o.graphview.placementstrategies;
+    opens com.dawist_o.graphview.placementstrategies;
+
+    exports com.dawist_o.graphview.edge;
+    opens com.dawist_o.graphview.edge;
+
+    exports com.dawist_o.graphview.vertex;
+    opens com.dawist_o.graphview.vertex;
+
     exports com.dawist_o.graphview;
     opens com.dawist_o.graphview;
 
